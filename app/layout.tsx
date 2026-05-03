0c2e899e55f1fabc71c233643df424b2379759d6
thulani.molefi@gmail.com <thulani.molefi@gmail.com>
2026-05-03 14:19:57 +0200

    fix duplicate prop build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Barlow:wght@400;500;600&family=Barlow+Condensed:wght@500;600;700&display=swap" rel="stylesheet"/>
-      </head>
       <body>{children}</body>
     </html>
   )
